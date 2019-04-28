01889c8f7d89bfbe98622e015f2a7fe466c73ed4
mateuszp <mpftp10@gmail.com>
2019-04-28 22:14:46 +0200

    fixed method when new boxes count > prev boxes count

--- object_tracking/object_tracking.py
+++ object_tracking/object_tracking.py
@@ -9,15 +9,13 @@ def assign_ids(boxes, prev=None):
 
     if prev is not None:
         indices = np.arange(0, prev.shape[0])
-        while centroids.size > 0:
+        while centroids.size > 0 and indices.size > 0:
             candidates = np.ones((prev.shape[0], 2)) * np.iinfo(int).max
-
             for i in range(0, indices.shape[0]):
                 pos = np.argmin(centroid.euclidean_distance(prev[indices[i]], centroids))
 
                 [dist] = centroid.euclidean_distance(prev[indices[i]], [centroids[pos]])
                 candidates[indices[i]] = np.array([pos, dist])
-
             ind = np.argmin(candidates[:, 1]).astype(int)
             new[ind] = centroids[candidates[ind][0].astype(int)]
 
@@ -25,4 +23,6 @@ def assign_ids(boxes, prev=None):
             for i in range(0, indices.shape[0]):
                 if indices[i] == ind:
                     indices = np.delete(indices, i)
+        if centroids.size > 0:
+            new = np.vstack((new, centroids))
         return new

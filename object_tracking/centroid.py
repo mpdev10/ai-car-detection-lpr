cb12bccdaeefc87d25749ba68e477040ed3793df
mateuszp <mpftp10@gmail.com>
2019-04-28 14:13:04 +0200

    defined centroid computation and euclidean distance

---
+++ object_tracking/centroid.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def compute_centroids(boxes):
+    centroids = np.vstack(((boxes[:, 2] + boxes[:, 0]) / 2, (boxes[:, 3] + boxes[:, 1]) / 2))
+    return centroids.transpose()
+
+
+def euclidean_distance(p_from, p_to):
+    return np.sum(np.abs(p_from - p_to), axis=1)

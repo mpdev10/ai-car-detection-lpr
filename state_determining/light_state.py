a1a0d76528a473d40e1f6c3c830b7ebaf499ec79
mateuszp <mpftp10@gmail.com>
2019-05-20 21:33:50 +0200

    defined determine_light_on method

--- state_determining/light_state.py
+++ state_determining/light_state.py
@@ -5,3 +5,7 @@ import numpy as np
 
 def light_level(image):
     return np.average(cv2.cvtColor(image, cv2.COLOR_RGB2GRAY))
+
+
+def determine_light_on(image, threshold):
+    return light_level(image) >= threshold
